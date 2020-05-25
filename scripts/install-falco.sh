b68d3c2ec163b9e5ddd07e176a18fd4c18dba054
James Strong <strong.james.e@gmail.com>
2020-05-25 14:18:29 -0400

    syntax error in falco systemd

--- scripts/install-falco.sh
+++ scripts/install-falco.sh
@@ -1,31 +1,44 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -o pipefail
+set -o nounset
+set -o errexit
 
 TEMPLATE_DIR=${TEMPLATE_DIR:-/tmp/worker}
 
 echo "##########################################################"
 echo "#                 INSTALLING FALCO                       #"
 echo "##########################################################"
-# Falco install instructions https://falco.org/docs/installation/
-
-# Trust the falcosecurity GPG key and configure the yum repository:
-sudo rpm --import $TEMPLATE_DIR/falcosecurity-3672BA8F.asc
-
-sudo cp $TEMPLATE_DIR/falcosecurity-rpm.repo /etc/yum.repos.d/falcosecurity.repo
-
-#Ensuring DKMS is installed
-sudo yum install epel-release
-
 # Update the OS to begin with to catch up to the latest packages.
 sudo yum update -y
 
+# Falco install instructions https://falco.org/docs/installation/
+
+# Trust the falcosecurity GPG key and configure the yum repository
+echo "Importing Key for yum repo"
+sudo rpm --import $TEMPLATE_DIR/falcosecurity-3672BA8F.asc
+
+echo "Adding Falco Repo"
+sudo cp $TEMPLATE_DIR/falcosecurity-rpm.repo /etc/yum.repos.d/falcosecurity.repo
+
+#Ensuring DKMS is installed per https://aws.amazon.com/premiumsupport/knowledge-center/ec2-enable-epel/
+echo "Ensuring DKMS is installed"
+sudo curl -o /tmp/epel.rpm  https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum install -y /tmp/epel.rpm
+
 # Install kernel headers:
+echo "Installing Kernel Headers"
 sudo yum -y install kernel-devel-$(uname -r)
 
 # Installing falco
+echo "Installing Falco"
 sudo yum -y install falco
 
-sudo mv $TEMPLATE_DIR/falco.service /etc/systemd/system/falco.service
+echo "Adding Falco systemd service file"
+sudo cp $TEMPLATE_DIR/falco.service /etc/systemd/system/falco.service
 
+echo "Reloading systemd"
 sudo systemctl daemon-reload
+echo "Enable Falco"
 sudo systemctl enable falco
-sudo systemctl start falco
+
